12a980744de2912ad9b35980cde249b25bd82a37
Axel Segovia <axel.segovia@Axels-MacBook-Pro.local>
2025-08-31 18:42:37 -0600

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  typescript:{
+    typeCheck:true
+  },
+
+  css: ["~/assets/base.css"],
+  modules: ["@nuxt/icon", "@nuxt/fonts"],
+
+  icon: {
+    customCollections: [
+      {
+        prefix: 'ci',
+        dir: './app/assets/icons',
+      },
+    ],
+    collections: ["mdi"],
+    size: "24px",
+    clientBundle: {
+      scan: true,
+    },
+  },
+  fonts: {
+    provider: 'google',
+    families: [
+      {
+        name: "Rubik", weights: [400, 500, 600, 700, 800]
+      }
+    ],
+  },
+});
